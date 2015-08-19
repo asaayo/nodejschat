6812fab55f8384dc4eaad5df34a1a44d5de917be
Brandon Trumble <brandon.trumble@gmail.com>
2015-08-19 15:31:55 -0400

    heroku and its dynamic ports

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.set('port', (process.env.PORT || 5000));
+var appPort = process.env.PORT;
 
 app.get('/', function(request, response){
 	res.send('<h1>Hello World</h1>');
 });
 
-http.listen(app.port, function(){
+http.listen(appPort, function(){
 	console.log('listening on *:3000');
 });
