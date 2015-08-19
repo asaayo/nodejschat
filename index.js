1794cfe360b6cb003f82c9f7ad377039a821b2cc
Brandon Trumble <brandon.trumble@gmail.com>
2015-08-19 16:10:48 -0400

    rough chat

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
-var appPort = process.env.PORT;
+var appPort = process.env.PORT || 5000;
 
 app.get('/', function(request, response){
-	response.send('<h1>Hello World</h1>');
-	response.end();
+	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('chat message', function(message){
+		io.emit('chat message', message);
+		console.log(message);
+	});
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
 
 http.listen(appPort, function(){
-	console.log('listening on *:3000');
+	console.log('listening on localhost:' + appPort);
 });
