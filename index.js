acc0b7993a3253444c1052d54916347a5ad694a4
Brandon Trumble <brandon.trumble@gmail.com>
2015-08-21 14:13:41 -0400

    Comments

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ var io = require('socket.io')(http);
 app.set('port', (process.env.PORT || 5000));
 var appPort = process.env.PORT || 5000;
 
+//Route for index
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
 });
 
+//Route for css
 app.get('/public/style.css', function(request, response){
 	response.sendFile(__dirname + '/public/style.css');
 });
 
+//Socket handling
 io.on('connection', function(socket){
+	//Handling for user connect messages
 	socket.on('statusMessage', function(message){
-		console.log(message);
 		io.emit('statusMessage', message);
 	});
+	//Handling for chat messages
 	socket.on('chatMessage', function(message){
-		console.log(JSON.parse(message));
 		io.emit('chatMessage', message);
 	});
 	socket.on('disconnect', function(){
@@ -27,6 +30,7 @@ io.on('connection', function(socket){
 	});
 });
 
+//Starts the node server
 http.listen(appPort, function(){
 	console.log('listening on localhost:' + appPort);
 });
